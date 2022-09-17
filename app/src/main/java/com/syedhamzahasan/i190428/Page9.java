b1cafeec0c656460b23561a0efdefef551d68ed3
Syed19982 <syedhasan3112@gmail.com>
2022-09-17 05:51:25 -0700

    done

---
+++ app/src/main/java/com/syedhamzahasan/i190428/Page9.java
@@ -0,0 +1,14 @@
+package com.syedhamzahasan.i190428;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page9 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page9);
+    }
+}
