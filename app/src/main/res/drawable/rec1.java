36666225d0e0f5788ee38f31b275d67ab62a7de9
Syed19982 <syedhasan3112@gmail.com>
2022-09-17 03:18:48 -0700

    done

---
+++ app/src/main/res/drawable/rec1.java
@@ -0,0 +1,14 @@
+package drawable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class rec1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rec1);
+    }
+}
