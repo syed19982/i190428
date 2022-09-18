b1d0102d0079424430f7c584b092ad39292859df
Syed19982 <syedhasan3112@gmail.com>
2022-09-18 06:18:49 -0700

    done

--- app/src/main/java/com/syedhamzahasan/i190428/Page8.java
+++ app/src/main/java/com/syedhamzahasan/i190428/Page8.java
@@ -1,14 +1,14 @@
-package drawable;
+package com.syedhamzahasan.i190428;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class rec1 extends AppCompatActivity {
+public class Page8 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rec1);
+        setContentView(R.layout.activity_page8);
     }
 }
